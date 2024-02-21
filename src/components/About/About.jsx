ed1856e1323bb0421acf5c2fc5fe7d8ed3eaab3c
Hadi Jamaludin <hadijmldn@gmail.com>
2024-02-21 19:38:35 +0700

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import CarPng from "../../assets/car1.png";
+
+const About = () => {
+  return (
+    <div className="dark:bg-dark bg-slate-100 sm:min-h-[600px] sm:grid sm:place-items-center duration-300">
+      <div className="container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
+          <div data-aos="slide-right" data-aos-duration="1500">
+            <img
+              src={CarPng}
+              alt=""
+              className="sm:scale-125 sm:-translate-x-11 max-h-[300px] drop-shadow-[2px_10px_6px_rgba(0,0,0,0.50)]"
+            />
+          </div>
+          <div>
+            <div className="space-y-5 sm:p-16 pb-6">
+              <h1
+                data-aos="fade-up"
+                className="text-3xl sm:text-4xl font-bold font-serif"
+              >
+                About us
+              </h1>
+              <p data-aos="fade-up" className="leading-8 tracking-wide">
+                Welcome to Arkmin car rental, your premier destination for
+                hassle-free car rentals!
+              </p>
+              <p data-aos="fade-up">
+                At Arkmin car rental, we understand the importance of reliable
+                transportation when it comes to exploring new destinations or
+                simply getting around town. Whether you're a local resident in
+                need of a temporary vehicle or a traveler looking to discover
+                the beauty of our region, we've got you covered with our wide
+                range of rental options.
+              </p>
+              <button data-aos="fade-up" className="button-outline">
+                Get Started
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
